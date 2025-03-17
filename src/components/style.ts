93be93e66d01ce669cd761590f83e7a0150a4c6a
Yang⭐ <78190786+313yang@users.noreply.github.com>
2025-03-18 00:07:22 +0900

    Feat: added font, modified header style

--- src/components/style.ts
+++ src/components/style.ts
@@ -2,12 +2,14 @@ import { styled } from "styled-components";
 
 export const HeaderComponent = styled.header`
   display: flex;
-  padding-left: 220px;
-  margin-top: 40px;
-  margin-bottom: 40px;
-  width: calc(100% - 220px);
+  align-items: center;
+  padding: 24px 160px 24px;
   > h1 {
     ${({ theme }) => theme.typography.title1};
     color: ${({ theme }) => theme.colors.text.primary};
+  }
+  > nav {
+    display: flex;
+    align-items: center;
   }
 `;
